1aae8f3b1f3171c903c832cc617a304f70b5e22d
davidotieno <davidotieno473@gmail.com>
2023-10-28 14:03:26 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import blogData from "../data/blog";
-import Header from "./Header.js"
-import About from "./About.js"
+import Header from "./Header"
+import About from "./About";
 import ArticleList from "./ArticleList";
 
 console.log(blogData);
@@ -17,3 +17,4 @@ function App() {
     </div>
   );
 }
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header(name){
+    return (
+    <header>{name}</header>   
+    )
+
+}
+export default Header;
